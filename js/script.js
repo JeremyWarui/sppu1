c07e76eae956293a9b814bbabfd69bab9f2cd39e
Jeremy <jerywarne7@gmail.com>
2022-06-08 23:06:46 +0300

    editign the navigation bar

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
-let menu =document.querySelector('#menubar');
-let navbar=document.querySelector('.navbar')
+let menu = document.querySelector("#menubar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick=()=>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-  const open=document.getElementById('open');
-  const modal_container=document.getElementById('modal-container');
-  const close=document.getElementById('close');
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal-container");
+const close = document.getElementById("close");
 
-  open.addEventListener('click',()=>{
-    modal_container.classList.add('show');
-  });
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
 
-  close.addEventListener('click',()=>{
-    modal_container.classList.remove('show');
-  });
-  
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
 
-
-const productContainers = [...document.querySelectorAll('.department-content')];
-const nxtBtn = [...document.querySelectorAll('.next-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
+const productContainers = [...document.querySelectorAll(".department-content")];
+const nxtBtn = [...document.querySelectorAll(".next-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
 
 productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
