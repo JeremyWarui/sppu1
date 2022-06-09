09fafc325f6f598f891e8c3cd193e21a8ab2e36d
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-06-09 14:46:00 +0300

    side navbar update
    Close side navbar after the link is clicked

--- js/sideNavbar.js
+++ js/sideNavbar.js
@@ -8,6 +8,7 @@ const menuIcon = document.querySelector(".navbar-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const sideNavBar = document.querySelector("#sideNavbar");
 const overlay = document.querySelector(".overlay");
+const sideNavBtns = document.querySelectorAll(".side-link");
 
 //open side navbar
 menuIcon.addEventListener("click", openSideNavbar);
@@ -15,15 +16,22 @@ menuIcon.addEventListener("click", openSideNavbar);
 //close side navbar
 closeBtn.addEventListener("click", closeSideNavbar);
 
-//openSidenavbar function
-function openSideNavbar() {
-  sideNavBar.classList.add("show");
-  overlay.classList.remove("hide");
+
+// hide sidenavbar when the navigation link is clicked
+const btns = [...sideNavBtns];
+for (const btn of btns) {
+    btn.addEventListener("click", closeSideNavbar);
 }
 
+//openSidenavbar function
+function openSideNavbar() {
+   sideNavBar.classList.add("show");
+   overlay.classList.remove("hide");
+}
+  
 //closeNavbar function
 function closeSideNavbar() {
-  // hide overlay and sidenavbar
+// hide overlay and sidenavbar
   overlay.classList.add("hide");
   sideNavBar.classList.remove("show");
-}
+ }
